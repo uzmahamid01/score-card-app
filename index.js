563851039f864d57196438abecfa4133feb4cf63
uzmahamid01 <uzma_hamid@tamu.edu>
2024-05-27 22:19:27 -0500

    made a new score board app

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let scoreEl = document.getElementById("score-el")
+
+let count = 0
+
+
+function homeplusone() {
+    count += 1
+    scoreEl.textContent = count
+}
+
+function homeplustwo() {
+    count += 2
+    scoreEl.textContent = count
+}
+
+function homeplusthree() {
+    count += 3
+    scoreEl.textContent = count
+}
+
+let scoreEll = document.getElementById("score-ell")
+
+let count1 = 0
+
+
+function guestplusone() {
+    count1 += 1
+    scoreEll.textContent = count1
+}
+
+function guestplustwo() {
+    count1 += 2
+    scoreEll.textContent = count1
+}
+
+function guestplusthree() {
+    count1 += 3
+    scoreEll.textContent = count1
+}
